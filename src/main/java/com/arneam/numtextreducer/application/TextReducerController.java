1fe3526c6c2b0ee7e4a65bc5712d5b7a8b988647
Daniel Medeiros <daniel@arneam.com>
2019-11-02 09:59:56 -0300

    Add log4j/Kafka integration

--- src/main/java/com/arneam/numtextreducer/application/TextReducerController.java
+++ src/main/java/com/arneam/numtextreducer/application/TextReducerController.java
@@ -1,16 +1,19 @@
 package com.arneam.numtextreducer.application;
 
 import com.arneam.numtextreducer.domain.TextReducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("reduce")
+@Slf4j
 public class TextReducerController {
 
   @GetMapping("/{name}")
   @ResponseStatus(HttpStatus.OK)
   public int reduceToNumber(@PathVariable String name) {
+    log.info("calling 'TextReducer#reduceToNumber' with parameter: " + name);
     return TextReducer.instance().reduceToNumber(name);
   }
 
